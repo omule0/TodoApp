01387e4247b71ae5220fc75eae84ee8c7b8b5ea7
Michael <michaelmomule@gmail.com>
2023-05-04 20:35:44 +0300

    created tasks unique to users

--- TodoApp/todo/models.py
+++ TodoApp/todo/models.py
@@ -8,6 +8,8 @@ class Task(models.Model):
     name = models.CharField(max_length=255)
     due_date = models.DateField(null=True, blank=True)
     due_time = models.TimeField(null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
       
--- TodoApp/todo/views.py
+++ TodoApp/todo/views.py
@@ -4,19 +4,25 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.http import JsonResponse, HttpResponse
-from .models import Task
 from .forms import TaskForm
-# Create your views here.
+from .models import Task
+from django.utils import timezone
 
 def landing_page(request):
     return render(request, 'landing_page.html')
+
+@login_required
+def sticky_notes(request):
+    return render(request, 'sticky_notes.html')
 
 @login_required
 def home(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            task = form.save()
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
             return JsonResponse({
                 'name': task.name,
                 'due_date': task.due_date.strftime('%Y-%m-%d') if task.due_date else '',
@@ -24,12 +30,8 @@ def home(request):
             })
     else:
         form = TaskForm()
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(user=request.user)
     return render(request, 'home.html', {'tasks': tasks, 'form': form})
-
-@login_required
-def sticky_notes(request):
-    return render(request, 'sticky_notes.html')
 
 
 @login_required
@@ -37,10 +39,15 @@ def add_task(request):
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            task = form.save()
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
             return JsonResponse({
                 'name': task.name,
                 'due_date': task.due_date.strftime('%Y-%m-%d') if task.due_date else '',
                 'due_time': task.due_time.strftime('%H:%M:%S') if task.due_time else '',
             })
-    return JsonResponse({'error': 'Invalid request'})
+        else:
+            return JsonResponse({'error': 'Invalid form data'})
+    else:
+        return JsonResponse({'error': 'Invalid request'})
