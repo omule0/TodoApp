d5a4c277ef90c20066bdaf89d715dfc70b936a41
Michael <michaelmomule@gmail.com>
2023-05-04 19:48:42 +0300

    fixed signup page

--- TodoApp/Users/views.py
+++ TodoApp/Users/views.py
@@ -2,31 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm
 from django.contrib.auth.decorators import login_required
-from todo.models import Owner
-from django.contrib.auth import login
-from django.db import IntegrityError
-from django.db.models import F
-from .models import Owner
 
-def register(request):
-    """SignUp page view that signs up new user to the system, according to given information."""
+def register(response):
+    if response.method == "POST":
+	    form = UserRegisterForm(response.POST)
+	    if form.is_valid():
+	        form.save()
 
-    if request.method == 'POST':
-        form = UserRegisterForm(request.POST)  # Instantiate UserRegisterForm with request.POST data
-        if form.is_valid():  # Check if form data is valid
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            try:
-                user = Owner.objects.create_user(username, email, password)
-                login(request, user)
-                return redirect('login')
-            except IntegrityError:
-                appStatus = "Oops! It seems like this username is taken, please choose another username."
-                return render(request, 'Users/register.html', {'form': form, 'status': appStatus})
+	    return redirect("/home")
     else:
-        form = UserRegisterForm()  # Instantiate an empty UserRegisterForm
-    return render(request, 'Users/register.html', {'form': form})
+	    form = UserRegisterForm()
+    return render(response, "Users/register.html", {"form":form})
 
 
 @login_required
--- TodoApp/todo/models.py
+++ TodoApp/todo/models.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-from Users.models import Owner
+from django.db import models
 
 
 class Task(models.Model):
     name = models.CharField(max_length=255)
     due_date = models.DateField(null=True, blank=True)
     due_time = models.TimeField(null=True, blank=True)
-
     def __str__(self):
         return self.name
       
