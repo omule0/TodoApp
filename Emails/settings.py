51acba959206ad08188238554ca7577971f53dcc
Opio Douglas <dopio8081@gmail.com>
2023-05-28 13:08:37 +0000

    updated setup

--- Emails/settings.py
+++ Emails/settings.py
@@ -3,3 +3,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER ='dopio8081@gmail.com'
 EMAIL_HOST_PASSWORD ='Opio777@'
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
